d928f90c95586045da9b31ce166b3e75ccdf25be
Delaw54 <ogheneovodelaw@gmail.com>
2025-07-14 17:46:14 -0700

    Fix setup.py to remove Cython, enable pip install

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='house_price_cleaner',
-    ext_modules=cythonize("house_price_cleaner/cleaner.py"),
-    packages=['house_price_cleaner'],
+    version='0.1',
+    packages=find_packages(),
     install_requires=[
-	'pandas',
-	'numpy',
-	'scikit-learn',
-	'category_encoders',
-	'xgboost' ],
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'category_encoders',
+        'xgboost'
+    ],
     zip_safe=False,
 )
 
