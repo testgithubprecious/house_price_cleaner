2e345a0a90f9dbbd60d4380183964646725efde9
Delaw54 <ogheneovodelaw@gmail.com>
2025-07-15 02:48:55 -0700

    Refactor to use cleaner.py instead of compiled .so file

---
+++ house_price_cleaner/__init__.py
@@ -0,0 +1,2 @@
+from .cleaner import HousePricePreprocessor
+
--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
 
 setup(
-    name='house_price_cleaner',
-    ext_modules=cythonize([
-        Extension("house_price_cleaner.cleaner", ["house_price_cleaner/cleaner.pyx"])
-    ]),
-    packages=['house_price_cleaner'],
+    name="house_price_cleaner",
+    version="0.1",
+    packages=find_packages(),
     install_requires=[
-        'pandas',
-        'numpy',
-        'scikit-learn',
-        'category_encoders',
-        'xgboost'
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "xgboost",
+        "category_encoders"
     ],
     zip_safe=False,
 )
